350fac3929e0152cafd6806554a069b0872d0f70
Oscar Suarez Nava <81348068+OscarSnva15@users.noreply.github.com>
2022-02-17 19:12:25 -0600

    basckground-subtraction-cameras

--- batch_queues_bkg_sub.py
+++ batch_queues_bkg_sub.py
@@ -1,7 +1,9 @@
 import os
 import cv2
-from multiprocessing import Process, Queue, current_process, freeze_support
+import time
 
+
+from multiprocessing import Process, Queue, current_process, freeze_support
 
 def recovery_video_files( path="." ):
     videos_to_processs = []
@@ -10,8 +12,12 @@ def recovery_video_files( path="." ):
             # Process only video file not previously processed
             videos_to_processs.append(x)
 
-    #print("\n".join(videos_to_processs))
+    print("*"*10)
+    print("\n".join(videos_to_processs))
+    print("*"*10)
+    
     return videos_to_processs
+
 #
 # Function run by worker processes
 #
@@ -19,9 +25,13 @@ def worker(input, output):
     for video in iter(input.get, 'STOP'):
         result = compute_video_background(video)
         output.put(result)
+        output("  ")
 
 def compute_video_background( video_to_process ):
+    size_video =  os.path.getsize(video_to_process)
+    print("initializing process {} for video {} with size {}".format(current_process().name, video_to_process,size_video) )
 
+    tic = time.time()
     out_background_filename = video_to_process.replace("mpeg","png")
 
     # create VideoCapture object for further video processing
@@ -29,8 +39,7 @@ def compute_video_background( video_to_process ):
     # check video capture status
     if not captured_video.isOpened:
         print("Unable to open: " + video_to_process)
-        return '%s says ERROR %s %s on generate %s' % \
-        (current_process().name, compute_video_background.__name__, video_to_process, out_background_filename)
+        return "%s says ERROR %s %s on generate %s".format(current_process().name, compute_video_background.__name__, video_to_process, out_background_filename)
 
     # instantiate background subtraction
     background_subtr_method = cv2.bgsegm.createBackgroundSubtractorGSOC()
@@ -52,17 +61,18 @@ def compute_video_background( video_to_process ):
         foreground_mask = background_subtr_method.apply(frame)
         # obtain the background without foreground mask
         background_img = background_subtr_method.getBackgroundImage()
+    
+    toc = time.time()
 
-
-    return '%s says that %s %s generate %s' % \
-        (current_process().name, compute_video_background.__name__, video_to_process, out_background_filename)
+    return "{} finished video {} took %s seconds, size video {}".format(current_process().name,  video_to_process, out_background_filename, toc-tic)
 
 
 def batch_background_processing( num_process = 4):
 
-    print("batch_background_processing using {} process ...".format(num_process))
+    print("batch_background_processing using {} process".format(num_process))
 
     videos_to_processs = recovery_video_files()
+
     # IF videos is empty THEN
     #    WAIT 2 horas
     # ELSE
@@ -84,12 +94,9 @@ def batch_background_processing( num_process = 4):
     for i in range(len(videos_to_processs)):
         print('\t', done_queue.get())
 
-
-
     # Tell child processes to stop
     for i in range(num_process):
         task_queue.put('STOP')
-
 
 if __name__ == '__main__':
     freeze_support()
@@ -97,6 +104,3 @@ if __name__ == '__main__':
     NUMBER_OF_PROCESSES = 8
 
     batch_background_processing( NUMBER_OF_PROCESSES )
-
-    
-    
